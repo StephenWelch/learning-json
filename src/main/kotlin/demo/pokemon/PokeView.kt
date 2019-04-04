4d8c25d5ba86ed711cd57fce0a39ad877a39da97
Stephen Welch <stephenwelchva@gmail.com>
2019-04-04 14:12:01 -0400

    Working JavaFX

--- src/main/kotlin/demo/pokemon/PokeView.kt
+++ src/main/kotlin/demo/pokemon/PokeView.kt
@@ -1,12 +1,29 @@
 package demo.pokemon
 
+import com.github.salomonbrys.kotson.fromJson
+import com.google.gson.Gson
 import tornadofx.View
+import tornadofx.button
 import tornadofx.hbox
+import tornadofx.imageview
+import java.io.File
 
 class PokeView(): View() {
 
-    override val root = hbox {
+    val gson = Gson()
+    val json = File("src/main/resources/pokedex.json").readText(Charsets.UTF_8)
+    val pokedex = gson.fromJson<Pokedex>(json)
 
+    override val root = hbox {
+        imageview("http://www.serebii.net/pokemongo/pokemon/145.png")
+        button("Refresh").setOnAction {
+            for(pokemon in pokedex.pokemon) {
+                println("Creating ImageView for: " + pokemon.img)
+                imageview(pokemon.img)
+            }
+        }
     }
+
+
 
 }
